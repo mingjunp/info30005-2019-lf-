d272b3cd0364786059583b6f74674b4a50e9c415
Yang Zhao <yangzhao@student.unimelb.edu.au>
2019-04-13 21:34:22 +1000

    add controller and shema
    add controller and shema of toilets

--- controllers/controller_toilets.js
+++ controllers/controller_toilets.js
@@ -1,16 +1,37 @@
+const mongoose = require('mongoose');
 const Toilet = require("../models/toilet");
 
 module.exports.getToilet = function (req, res) {
-    Toilet.find({}, function (err, r) {
-        res.send(r);
+    Toilet.find(function (err, toilets) {
+        if(!err){
+            res.send(toilets);
+        }else{
+            res.sendStatus(404);
+        }
     });
 };
 module.exports.createToilet = function (req, res) {
     const toilet = new Toilet({
-        location: "10.888758,88.08498",
-        picture: "988882",
-        //测试版
+        "userName":req.body.userName,
+        "toiletName":req.body.toiletName,
+        "toiletPicture":req.body.toiletPicture,
+        "location":{
+            lat:req.body.lat,
+            lon:req.body.lon
+        },
+        "female":req.body.female,
+        "male":req.body.male,
+        "wheelchair":req.body.wheelchair,
+        "babyFacil":req.body.babyFacil,
+        "shower":req.body.shower,
+        "aveRating":req.body.aveRating
     });
-    toilet.save().then(res.send('successful! ' + toilet));
+    toilet.save(function(err,newToilet){
+        if(!err){
+            res.send(newToilet);
+        }else{
+            res.sendStatus(400);
+        }
+    });
 };
 
--- models/toilet.js
+++ models/toilet.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const toiletSchema = new mongoose.Schema({
-    location: String,
-    picture: String,
-
+    "userName":String,
+    "toiletName":String,
+    "toiletPicture":String,
+    "location":{lat:String, lon:String},
+    "female":String,
+    "male":String,
+    "wheelchair":String,
+    "babyFacil":String,
+    "shower":String,
+    "aveRating":Number
 });
 
 module.exports = mongoose.model("toilet", toiletSchema, "toilets");
