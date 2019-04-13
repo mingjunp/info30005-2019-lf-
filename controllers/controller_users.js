8915289e52fd7ea04b35aa6630f9b04b5fe303be
sddua <sddua@student.unimelb.edu.au>
2019-04-13 23:46:27 +1000

    [users]

--- controllers/controller_users.js
+++ controllers/controller_users.js
@@ -1,11 +1,27 @@
 const User = require("../models/user");
 
 module.exports.getUser = function (req, res) {
-    User.find({}, function (err, r) {
-        res.send(r);
+    User.find(function(err,users){
+        if(!err){
+            res.send(users);
+        }else{
+            res.sendStatus(404);
+        }
     });
 };
-module.exports.createUser = function (req, res) {
 
+module.exports.createUser = function (req, res) {
+ 	const user = new User({
+        "userName":req.body.userName,
+        "password":req.body.password,
+        "phoneNumber":req.body.phoneNumber
+    });
+    user.save(function(err,newUser){
+        if(!err){
+            res.send(newUser);
+        }else{
+            res.sendStatus(400);
+        }
+    });
 };
 
--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    userName: String,
-
-
+    "userName":String,
+    "password":String,
+    "phoneNumber":String
 });
 
 module.exports = mongoose.model("user", userSchema, "users");
