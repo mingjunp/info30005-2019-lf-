fc9ec36e9a281e0358f299196cdaf16c0189a69b
Yang Zhao <yangzhao@student.unimelb.edu.au>
2019-05-10 17:56:09 +1000

    add checkLogin.js
    add checkLogin.js

---
+++ public/JS/checkLogin.js
@@ -0,0 +1,28 @@
+function checkLogin() {
+    $.ajax({
+        url:'http://192.168.1.106:3000/api/users/checkLogin',
+        type:'get',
+        contentType: "application/json; charset=utf-8",
+        dataType:'json',
+        data:JSON.stringify(""),
+        success:function(result){
+            // login fail
+            if (result.errno == -1){
+                $('#nav-right').children('li').remove();
+                $('#nav-right').append($(`<li><a data-toggle="modal" data-target="#login" href=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a data-toggle="modal" data-target="#register" href=""><span class="glyphicon glyphicon-user"></span>Sign up</a></li>`));
+
+            }
+            // login success
+            if (result.errno == 0){
+                $('#nav-right').children('li').remove();
+                $('#nav-right').append($(`<li><p class="navbar-text">Hi, <a href="#" class="navbar-link">`+ result.message +`</a></p></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>`));
+            }
+            console.log(result);
+        },
+        error:function(e){
+            console.log(e);
+        },
+    });
+}
